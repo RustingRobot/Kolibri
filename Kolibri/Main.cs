c7e61a904fa729304e3f138c903e40afb3502aab
RustingRobot <rusting.robot.scratch@gmail.com>
2020-12-22 04:17:21 +0100

    Clean Up!

--- Kolibri/Main.cs
+++ Kolibri/Main.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Kolibri
 {
-    public class Game1 : Game
+    public class Main : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public Main()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -18,35 +18,34 @@ namespace Kolibri
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            GraphicsDevice.Clear(Color.DarkGray);
             base.Draw(gameTime);
+        }
+    }
+
+    public static class Program
+    {
+        static void Main()
+        {
+            using (var game = new Main())
+                game.Run();
         }
     }
 }

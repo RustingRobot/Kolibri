857bd022c462d1549c32a96ea8501fff30666739
RustingRobot <rusting.robot.scratch@gmail.com>
2021-02-03 00:55:04 +0100

    started implementing bresenham's algorithm

--- Kolibri/Source/Workspace/Canvas.cs
+++ Kolibri/Source/Workspace/Canvas.cs
@@ -16,27 +16,67 @@ namespace Kolibri.Source.Workspace
         UInt32[] pixels;
         Random rnd = new Random();
         //List<Vector2> mousePoints = new List<Vector2>();
-        Vector2 delta;
         public Canvas(Vector2 POS, Vector2 DIM)
         {
             pos = POS;
             dim = DIM;
             canvas = new Texture2D(Globals.graphicsDevice, (int)dim.X, (int)dim.Y, false, SurfaceFormat.Color);
             pixels = new UInt32[(int)dim.X * (int)dim.Y];
-            for (int i = 0; i < pixels.Length; i++) //set all pixel to white
+            for (int i = 0; i < pixels.Length; i++) //set all pixels to white
             {
                 pixels[i] = 0xFFFFFFFF;
             }
         }
         public void Update(Vector2 OFFSET)
         {
+            //drawLine(new Vector2(65, 65), Globals.mouse.newMousePos, Color.Red);
             Globals.graphicsDevice.Textures[0] = null;
-            delta = Globals.mouse.newMousePos - Globals.mouse.oldMousePos;
             if (Globals.GetBoxOverlap(pos, dim, Globals.mouse.newMousePos, Vector2.Zero) && Globals.mouse.LeftClickHold() && !Globals.dragging) //only calculate if the mouse is supposed to draw on the canvas
             {
-                pixels[(int)(Globals.mouse.newMousePos.Y - pos.Y) * (int)dim.X + (int)(Globals.mouse.newMousePos.X - pos.X)] = 0xFF000000;  //set one pixel to black
+                drawLine(Globals.mouse.oldMousePos, Globals.mouse.newMousePos, Color.CornflowerBlue);
             }
             canvas.SetData<UInt32>(pixels, 0, (int)dim.X * (int)dim.Y);
+        }
+
+        public void setPixel(Vector2 position, Color color)
+        {
+            if((int)(position.Y - pos.Y) * (int)dim.X + (int)(position.X - pos.X) < pixels.Length && (int)(position.Y - pos.Y) * (int)dim.X + (int)(position.X - pos.X) > 0)
+                pixels[(int)(position.Y - pos.Y) * (int)dim.X + (int)(position.X - pos.X)] = (uint)((color.A << 24) | (color.B << 16) | (color.G << 8) | (color.R << 0));
+        }
+
+        public void drawLine(Vector2 pos0, Vector2 pos1, Color color)   //implementation of bresenham's line algorithm
+        {
+            float x = pos0.X, y = pos0.Y;
+            float dx = pos1.X - pos0.X;
+            float dy = pos1.Y - pos0.Y;
+            bool xFaster = dx > dy;
+            float f = (xFaster) ? dx / 2 : dy / 2;
+            setPixel(pos0, color);
+
+            while (x != pos1.X && y != pos1.Y) 
+            {
+                if (xFaster)
+                {
+                    x++;
+                    f -= dy;
+                    if (f < 0)
+                    {
+                        y++;
+                        f += dx;
+                    }
+                }
+                else
+                {
+                    y++;
+                    f -= dx;
+                    if (f < 0)
+                    {
+                        x++;
+                        f += dy;
+                    }
+                }
+            setPixel(new Vector2(x, y), color);
+            }
         }
 
         public void Draw(Vector2 OFFSET)

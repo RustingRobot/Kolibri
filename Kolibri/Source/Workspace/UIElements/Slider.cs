8574219ca5f21f6d46333ecfd17a7fce483438c6
RustingRobot <rusting.robot.scratch@gmail.com>
2021-03-15 01:46:59 +0100

    colorPicker changes

--- Kolibri/Source/Workspace/UIElements/Slider.cs
+++ Kolibri/Source/Workspace/UIElements/Slider.cs
@@ -3,6 +3,7 @@ using Kolibri.Engine;
 using Microsoft.Xna.Framework;
 using Kolibri.Source.Workspace;
 using System.Diagnostics;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Kolibri.Source.Workspace.UIElements
 {
@@ -42,8 +43,10 @@ namespace Kolibri.Source.Workspace.UIElements
             posMarker.X = pos.X + (dim.X - handleWidth) / (end - start) * value;
             if (dragged || (MouseHover() && Globals.mouse.LeftClickHold() && window.MouseInWindow()))
             {
-                value = (int)(Globals.mouse.newMousePos.X - pos.X);
-                value = Math.Clamp(value, start, end);
+                value = (int)((Globals.mouse.newMousePos.X - pos.X) * (end / dim.X) );
+                if (value > end) value = end;
+                if (value < start) value = start;
+                //value = Math.Clamp(value, start, end);
                 dragged = true;
             }
 
@@ -58,12 +61,11 @@ namespace Kolibri.Source.Workspace.UIElements
        {
             base.Draw(OFFSET);
             Globals.primitives.DrawRect(pos, new Vector2(dim.X,dim.Y), color);
-
             //Marker
             Globals.primitives.DrawRect(posMarker, new Vector2(handleWidth, dim.Y), new Color(39, 44, 48));
             Globals.primitives.DrawTxt(value.ToString(), pos + new Vector2(dim.X / 2 - (Globals.font.MeasureString(value.ToString()).X * Globals.fontSize.X) / 2, 0), Globals.fontSize * 0.9f, new Color(245, 255, 250));
 
-        } 
+        }
 
     }
 
--- Kolibri/Source/Workspace/Windows/ColorPickerWindow.cs
+++ Kolibri/Source/Workspace/Windows/ColorPickerWindow.cs
@@ -1,73 +1,80 @@
 
-﻿using System;
 using Kolibri.Engine;
 using Microsoft.Xna.Framework;
-using Kolibri.Source.Workspace;
-
-
+using System;
+using System.Linq;
 
 namespace Kolibri.Source.Workspace.UIElements
 {
     class ColorPickerWindow : Window
     {
-        private Button r, g, b, rgb;
+        private Button rgb, hsv;
         public Color currentColor;
         Slider[] slider = new Slider[3];
-        private Label colorLabel;
+        Label[] sliderDesc = new Label[3];
         Textfield[] fields = new Textfield[3];
         private int redValue, greenValue, blueValue;
+        private bool rgbMode = true;
  
         public ColorPickerWindow(Vector2 POS, Vector2 DIM) : base(POS, DIM, "ColorPicker")
         {
-            //button for sliders
-            //rgb = new Button(PickColorRGB, this, new Vector2(10, 155), new Vector2(100, 100), "");
-            //rgb.normColor = new Color(0, 0, 0);
+            //button
+            rgb = new Button(changeMode, "rgb", this, new Vector2(60, 130), new Vector2(150, 20), "rgb mode");
+            rgb.txtColor = new Color(200, 200, 200);
+            hsv = new Button(changeMode, "hsv", this, new Vector2(215, 130), new Vector2(150, 20), "hsv mode");
+            hsv.txtColor = new Color(200, 200, 200);
             //slider red
-            slider[0] = new Slider(0,255,1,this,new Vector2(10,40),new Vector2(255,5),""); 
-            slider[0].color = new Color(255, 0, 0);
+            slider[0] = new Slider(0,255,1,this,new Vector2(60,40),new Vector2(255,15),""); 
             //slider green
-            slider[1] = new Slider(0, 255, 1, this, new Vector2(10, 80), new Vector2(255, 5), ""); 
-            slider[1].color = new Color(15, 105, 15);
+            slider[1] = new Slider(0, 255, 1, this, new Vector2(60, 70), new Vector2(255, 15), ""); 
             //slider blue
-            slider[2] = new Slider(0, 255, 1, this, new Vector2(10, 120), new Vector2(255, 5), ""); 
-            slider[2].color = new Color(0, 0, 255);
-            //lable
-            colorLabel = new Label(this, new Vector2(32, 252), new Vector2(90, 25), "Color:" + slider[0].getValue() + "," + slider[1].getValue() + "," + slider[2].getValue());
-            colorLabel.color = new Color(255, 255, 255);
+            slider[2] = new Slider(0, 255, 1, this, new Vector2(60, 100), new Vector2(255, 15), ""); 
             //textfields
-            fields[0] = new Textfield(this,new Vector2(320,40),new Vector2(40,18),"") { defaultContent = "0" };
-            fields[1] = new Textfield(this, new Vector2(320, 80), new Vector2(40, 18), "") { defaultContent = "0" };
-            fields[2] = new Textfield(this, new Vector2(320, 120), new Vector2(40, 18), "") { defaultContent = "0" };
+            fields[0] = new Textfield(this,new Vector2(325,38),new Vector2(40,20),"") { defaultContent = "0" };
+            fields[1] = new Textfield(this, new Vector2(325, 68), new Vector2(40, 20), "") { defaultContent = "0" };
+            fields[2] = new Textfield(this, new Vector2(325, 98), new Vector2(40, 20), "") { defaultContent = "0" };
             fields[0].numberField = true;
             fields[1].numberField = true;
             fields[2].numberField = true;
+            //slider descriptions
+            sliderDesc[0] = new Label(this, new Vector2(50, 48), Globals.fontSize, "R");
+            sliderDesc[1] = new Label(this, new Vector2(50, 78), Globals.fontSize, "G");
+            sliderDesc[2] = new Label(this, new Vector2(50, 108), Globals.fontSize, "B");
         }
         
-        public void PickColorRed()
+        void changeMode(string mode)
         {
-            currentColor = new Color(255, 0, 0); //red
-            slider[0].SetValue(255);
-            slider[1].SetValue(0);
-            slider[2].SetValue(0);
-        }
-        public void PickColorGreen()
-        {
-            currentColor = new Color(15, 105, 15); //green
-            slider[0].SetValue(15);
-            slider[1].SetValue(105);
-            slider[2].SetValue(15);
-        }
-        public void PickColorBlue()
-        {
-            currentColor = new Color(0, 0, 255); //blue
-            slider[0].SetValue(0);
-            slider[1].SetValue(0);
-            slider[2].SetValue(255);
-        }
-        public void PickColorRGB()
-        {
-            currentColor = new Color(slider[0].getValue(), slider[1].getValue(), slider[2].getValue()); //Chosen color with sliders
-            rgb.normColor = currentColor;
+            if(mode == "rgb")
+            {
+                sliderDesc[0].label = "R";
+                sliderDesc[1].label = "G";
+                sliderDesc[2].label = "B";
+
+                slider[0].end = 255;
+                slider[1].end = 255;
+                slider[2].end = 255;
+
+                rgbMode = true;
+
+                setColor(currentColor);
+            }
+            else
+            {
+                sliderDesc[0].label = "H";
+                sliderDesc[1].label = "S";
+                sliderDesc[2].label = "V";
+
+                slider[0].end = 360;
+                slider[1].end = 100;
+                slider[2].end = 100;
+
+                rgbMode = false;
+
+                float[] colorValues = RGBtoHSV((float)currentColor.R, (float)currentColor.G, (float)currentColor.B);
+                slider[0].value = (int)colorValues[0];
+                slider[1].value = (int)(colorValues[1] * 100);
+                slider[2].value = (int)(colorValues[2] * 100);
+            }
         }
 
         public void setColor(Color color)
@@ -79,36 +86,91 @@ namespace Kolibri.Source.Workspace.UIElements
         public override void Update(Vector2 OFFSET)
         {
             base.Update(OFFSET);
-            //rgb.Update(OFFSET);
-            slider[0].Update(OFFSET);
-            slider[1].Update(OFFSET);
-            slider[2].Update(OFFSET);
-            //rgb.Update(OFFSET);
-            colorLabel.Update(OFFSET);
-            colorLabel.label = "Color: " + redValue + "," + greenValue + "," + blueValue;
+            slider[0].Update(OFFSET); slider[1].Update(OFFSET); slider[2].Update(OFFSET);
             fields[0].Update(OFFSET); fields[1].Update(OFFSET); fields[2].Update(OFFSET);
-            currentColor = new Color(slider[0].getValue(), slider[1].getValue(), slider[2].getValue()); //Chosen color with sliders
-           //rgb.normColor = currentColor;
-            if (fields[0].content != "") redValue = int.Parse(fields[0].content);
-            if (fields[1].content != "") greenValue = int.Parse(fields[1].content);
-            if (fields[2].content != "") blueValue = int.Parse(fields[2].content);
-            fields[0].content = Convert.ToString(slider[0].getValue());
-            fields[1].content = Convert.ToString(slider[1].getValue());
-            fields[2].content = Convert.ToString(slider[2].getValue());
+            sliderDesc[0].Update(OFFSET); sliderDesc[1].Update(OFFSET); sliderDesc[2].Update(OFFSET);
+
+            if (rgbMode)
+                currentColor = new Color(slider[0].getValue(), slider[1].getValue(), slider[2].getValue()); //Chosen color with sliders
+            else
+                currentColor = HSVtoRGB(slider[0].getValue(), slider[1].getValue() / 100.0, slider[2].getValue() / 100.0);
+            if (fields[0].selected || fields[1].selected || fields[2].selected) setColor(new Color(int.Parse(fields[0].content), int.Parse(fields[1].content), int.Parse(fields[2].content)));
+            if (!fields[0].selected) fields[0].content = Convert.ToString(slider[0].getValue());
+            if (!fields[1].selected) fields[1].content = Convert.ToString(slider[1].getValue());
+            if (!fields[2].selected) fields[2].content = Convert.ToString(slider[2].getValue());
+
+            rgb.Update(OFFSET);
+            hsv.Update(OFFSET);
         }
 
         public override void Draw(Vector2 OFFSET, Color COLOR)
         {
             base.Draw(OFFSET, COLOR);
             beginWindowContent();
-            //rgb.Draw(OFFSET);
-            colorLabel.Draw(OFFSET);
-            slider[0].Draw(OFFSET);
-            slider[1].Draw(OFFSET);
-            slider[2].Draw(OFFSET);
+            slider[0].Draw(OFFSET); slider[1].Draw(OFFSET); slider[2].Draw(OFFSET);
             fields[0].Draw(OFFSET); fields[1].Draw(OFFSET); fields[2].Draw(OFFSET);
-            Globals.primitives.DrawRect(new Vector2(10,150 )+pos, new Vector2(100, 100), currentColor);
+            sliderDesc[0].Draw(OFFSET); sliderDesc[1].Draw(OFFSET); sliderDesc[2].Draw(OFFSET);
+            Globals.primitives.DrawRect(new Vector2(10,40) + pos, new Vector2(30, 75), currentColor);
+            rgb.Draw(OFFSET);
+            hsv.Draw(OFFSET);
             endWindowContent();
+        }
+
+        public Color HSVtoRGB(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+            if (hi == 0)
+                return new Color(v, t, p);
+            else if (hi == 1)
+                return new Color(q, v, p);
+            else if (hi == 2)
+                return new Color(p, v, t);
+            else if (hi == 3)
+                return new Color(p, q, v);
+            else if (hi == 4)
+                return new Color(t, p, v);
+            else
+                return new Color(v, p, q);
+        }
+
+        private float[] RGBtoHSV(float r, float g, float b)
+        {
+            r /= 255;
+            g /= 255;
+            b /= 255;
+
+            float Cmax = new float[] { r, g, b }.Max();
+            float Cmin = new float[] { r, g, b }.Min();
+            float delta = Cmax - Cmin;
+            float h;
+
+            if (delta == 0)
+            {
+                h = 0;
+            }
+            else if (Cmax == r)
+            {
+                h = 60 * ((g - b) / delta % 6);
+            }
+            else if (Cmax == g)
+            {
+                h = 60 * ((b - r) / delta + 2);
+            }
+            else
+            {
+                h = 60 * ((r - g) / delta + 4);
+            }
+
+            float s = (Cmax == 0) ? 0 : delta / Cmax;
+            float v = Cmax;
+
+            return new float[] { h, s, v };
         }
     }
 }
